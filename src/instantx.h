39b5fdf29aa785fff02eda7e1be66a40bffa05e4
Volkshash Core Dev <118804027+VolkshashDEV@users.noreply.github.com>
2023-02-21 15:06:41 +0200

    static const CAmount MIN_FEE
    Change the instantx transaction to VHH 100

--- src/instantx.h
+++ src/instantx.h
@@ -168,7 +168,7 @@ public:
 class CTxLockRequest
 {
 private:
-    static const CAmount MIN_FEE            = 0.0001 * COIN;
+    static const CAmount MIN_FEE            = 100 * COIN;
     /// If transaction has less or equal inputs than MAX_INPUTS_FOR_AUTO_IX,
     /// it will be automatically locked
     static const int MAX_INPUTS_FOR_AUTO_IX = 4;
